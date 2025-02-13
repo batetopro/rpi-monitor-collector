767e8d8286c3f078d92d72b0037e0a423576e942
Hristo Georgiev <hristo.i.georgiev@gmail.com>
2025-02-13 12:28:19 +0200

    Make DNS domains links in the admin change list

--- monitoring/network/admin.py
+++ monitoring/network/admin.py
@@ -1,14 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
+
 from network.models import DnsRecordModel, NeighbourModel
 
 
 class DnsAdminModel(admin.ModelAdmin):
     change_list_template = 'admin/dns_change_list.html'
 
-    list_display = ("address", "domain", )
+    list_display = ("address", "domain_link", )
     list_filter = ("address", )
     readonly_fields = ("address", "domain", )
     search_fields = ("domain", )
+
+    @admin.display(description="Domain", ordering='domain')
+    def domain_link(self, obj):
+        return format_html(
+            '<a href="http://' + obj.domain + '/" target="_blank">' +
+            obj.domain +
+            '</a>'
+        )
 
     def get_ordering(self, request):
         return ("address", )
